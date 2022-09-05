59fd22b0af188f60fc7bf22f3db982dd72bbbfd6
JSP Iwahashi <tsuyoshi.iwahashi@jspnet.co.jp>
2022-09-05 13:15:38 +0900

    電卓アプリにキー入力機能を追加

--- lib/view/calculator/calculator_page.dart
+++ lib/view/calculator/calculator_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:expressions/expressions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculatorPage extends StatefulWidget {
   const CalculatorPage({Key? key}) : super(key: key);
@@ -11,8 +14,24 @@ class CalculatorPage extends StatefulWidget {
 class _CalculatorPageState extends State<CalculatorPage> {
   static const _error = 'error';
   var _expression = '0';
+  var _focusNode = FocusNode();
+  var _timer;
 
-  void _addExpression(c) {
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(milliseconds: 16), (timer) {
+      _focusNode.requestFocus();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  void _addExpression(String c) {
     setState(() {
       switch (c) {
         case 'C':
@@ -44,7 +63,21 @@ class _CalculatorPageState extends State<CalculatorPage> {
           }
           break;
 
-        default:
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '.':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
           // 式が error または 0 なら空にしておく
           switch (_expression) {
             case _error:
@@ -52,6 +85,10 @@ class _CalculatorPageState extends State<CalculatorPage> {
               _expression = '';
           }
           _expression += c;
+          break;
+
+        default:
+          // 上記以外のキーは無視
           break;
       }
     });
@@ -71,62 +108,79 @@ class _CalculatorPageState extends State<CalculatorPage> {
       appBar: AppBar(
         title: const Text('Calculator'),
       ),
-      body: Container(
-        color: Colors.grey.shade200,
-        constraints: const BoxConstraints(maxHeight: double.infinity),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      _expression,
-                      style: const TextStyle(fontSize: 64.0),
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.end,
+      body: KeyboardListener(
+        focusNode: _focusNode,
+        onKeyEvent: (e) {
+          print(e);
+          if (e is KeyDownEvent) {
+            if (e.logicalKey == LogicalKeyboardKey.enter) {
+              _addExpression('=');
+            }
+            else if (e.logicalKey == LogicalKeyboardKey.keyC) {
+              _addExpression('C');
+            }
+            else {
+              _addExpression(e.character ?? '');
+            }
+          }
+        },
+        child: Container(
+          color: Colors.grey.shade200,
+          constraints: const BoxConstraints(maxHeight: double.infinity),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        _expression,
+                        style: const TextStyle(fontSize: 64.0),
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.end,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 4,
-                child: GridView.count(
-                  crossAxisCount: 4,
-                  childAspectRatio: aspectRatio,
-                  mainAxisSpacing: 4.0,
-                  crossAxisSpacing: 4.0,
-                  children: [
-                    '7', '8', '9', '÷', //
-                    '4', '5', '6', '×', //
-                    '1', '2', '3', '-', //
-                    'C', '0', '=', '+', //
-                  ]
-                      .map(
-                        (label) => GridTile(
-                          child: MaterialButton(
-                            color: Colors.white,
-                            onPressed: () {
-                              _addExpression(label);
-                            },
-                            child: Text(
-                              label,
-                              style: TextStyle(
-                                fontSize: 48,
-                                color: Colors.grey.shade600,
+                Expanded(
+                  flex: 4,
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    childAspectRatio: aspectRatio,
+                    mainAxisSpacing: 4.0,
+                    crossAxisSpacing: 4.0,
+                    children: [
+                      '7', '8', '9', '÷', //
+                      '4', '5', '6', '×', //
+                      '1', '2', '3', '-', //
+                      'C', '0', '=', '+', //
+                    ]
+                        .map(
+                          (label) => GridTile(
+                            child: MaterialButton(
+                              color: Colors.white,
+                              onPressed: () {
+                                _addExpression(label);
+                              },
+                              child: Text(
+                                label,
+                                style: TextStyle(
+                                  fontSize: 48,
+                                  color: Colors.grey.shade600,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      )
-                      .toList(),
+                        )
+                        .toList(),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
